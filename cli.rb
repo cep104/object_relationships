788aba6fe76fa10ca2ea0bf945e92311c524df72
cep104 <candicelizabethpeters@gmail.com>
2021-03-23 19:36:29 -0500

    organized notes

--- cli.rb
+++ cli.rb
@@ -1,33 +1,32 @@
 class CLI
-  def intro 
-    puts "Welcome to Starbucks, what is your name?"
-    input = gets.strip
-    customer = Customer.new(input)
-    greeting(customer)
-  end
+    def intro 
+        puts "Welcome to Starbucks, what is your name?"
+        input = gets.strip
+        customer = Customer.new(input)
+        greeting(customer)
+    end
 
-  def greeting(customer) 
-    puts "Cool, nice to meet you #{customer.name}. What would you like to order?"
-    input = gets.strip.split(", ")
-    make_order(input, customer)
-  end
+    def greeting(customer) 
+        puts "Cool, nice to meet you #{customer.name}. What would you like to order?"
+        input = gets.strip.split(", ")
+        make_order(input, customer)
+    end
 
-  def make_order(input, customer)
-new_order = Order.new(input[0], input[1], input[2])
-new_order.customer = customer
-new_order.barista = Barista.new("Jamie")
-# binding.pry
-print_order(new_order)
-  end
+    def make_order(input, customer)
+        new_order = Order.new(input[0], input[1], input[2])
+        new_order.customer = customer
+        new_order.barista = Barista.new("Jamie")
+        # binding.pry
+        print_order(new_order)
+    end
 
-  def print_order(order)
-
-  puts "here is your order #{order.customer.name}"
-  puts "~~~~~~~~~~~~~~~~~~~~~"
-  puts "Drink: #{order.drink}"
-  puts "Size: #{order.size}"
-  puts "Total: #{order.total}"
-  puts "~~~~~~~~~~~~~~~~~~~~~"
-  puts "prepared by #{order.barista.name}"
-end
+    def print_order(order)
+        puts "here is your order #{order.customer.name}"
+        puts "~~~~~~~~~~~~~~~~~~~~~"
+        puts "Drink: #{order.drink}"
+        puts "Size: #{order.size}"
+        puts "Total: #{order.total}"
+        puts "~~~~~~~~~~~~~~~~~~~~~"
+        puts "prepared by #{order.barista.name}"
+    end
 end
